7b975a8f5461cf70bd4e44ce462074092805cac2
valexyval <valentineekwurumadu@gmail.com>
2024-05-06 11:00:47 +0100

    changes

---
+++ function.dart
@@ -0,0 +1,3 @@
+void main(){
+  print("this is a print statement")
+}
--- hello_world.dart
+++ hello_world.dart
@@ -4,5 +4,5 @@ void main(){
 var nummy = stdin.readLineSync();
 var nummy2 = int.parse (nummy??'0') + 10;
 print("$nummy + 10 = $nummy2");
-
+name
 }
